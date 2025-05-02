fccf3c7f856f3bc55e7bab9dc47539be69aa6a0d
fahim-arif <fahim1.618555@gmail.com>
2025-05-02 10:02:53 +0600

    main: add devtool

--- src/main/java/com/fahim/springboot/demo/fahimfirstapp/rest/FunRestController.java
+++ src/main/java/com/fahim/springboot/demo/fahimfirstapp/rest/FunRestController.java
@@ -1,15 +1,25 @@
 package com.fahim.springboot.demo.fahimfirstapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    // FROM application.properties
+    @Value("${dev.name}")
+    private String devName;
+
     // EXPOSE / that returns Hello World
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World";
+        return String.format("Hello World %s", devName);
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run 4KM";
     }
 
 }
